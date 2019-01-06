29d08153a1e5b89bed32e8dc950b75fb56d1c47c
Pablo Sanchez Egido <pablosze@gmail.com>
2019-01-06 13:07:46 +0100

    Initial commit

---
+++ firebase-environments-starter/FirebaseEnvironments/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FirebaseEnvironments
+//
+//  Created by Pablo on 06/01/2019.
+//  Copyright © 2019 Pablo Sanchez Egido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController : UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
